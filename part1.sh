e31fe50f734ee58253b81b52a5100c87b9b84482
Eva HOUZARD <ehouzard@e3r5p4.42.fr>
2019-04-03 19:20:51 +0200

    +

--- part1.sh
+++ part1.sh
@@ -2,9 +2,21 @@
 echo "\033[33mConfiguration of the UNIX OS Ubuntu 18.4\033[0m"
 echo "\033[33mYOU MUST BE ROOT TO PERFORM THIS OPERATION\033[0m"
 echo"\033[31mPlease enter your personal username:\033[0m"
-read PERSO
-read -p "Configuration for $PERSO, press enter"
-sudo adduser $PERSO
+read -p 'your username: ' new_user
+while true; do
+  if ! [[ ${new_user} =~ ^[a-z0-9_-]{2,10}$ ]];
+  then
+    echo "\nThis is not a correct username"
+    echo "(Regex for a valid username : ^[a-z0-9_-]{2,10}$)"
+    echo "Please try again."
+    read -p 'your username: ' new_user
+    break
+   else
+     echo "Ok"
+   fi
+done
+read -p 'Configuration for $new_user, press enter'
+sudo adduser $new_user
 # update system
 echo "\033[33mAPT update/upgrade & installation of required packages\033[0m"
 apt-get update && apt-get upgrade -y
@@ -16,7 +28,7 @@ echo"set nu\nsyntax on\nset mouse=a\ncolo torte\n" > ~/.vimrc
 
 # create sudo user
 echo "\033[33mAdding your username to sudo group\033[0m"
-adduser $PERSO sudo
+adduser $new_user sudo
 
 # set static ip according to the v-host
 echo "\033[33mSetting up netplan...\033[0m"
@@ -26,7 +38,7 @@ chmod 644 /etc/netplan/50-cloud-init.yaml
 # Change SSH port
 echo "\033[33mSSH configuration:\033[0m"
 echo "\033[33mUncomment '# Port 22' on line 13: change the number to 2223\033[0m"
-read -p "Press enter to edit the config file"
+read -p 'Press enter to edit the config file'
 vim /etc/ssh/sshd_config
 service ssh restart
 
@@ -39,37 +51,27 @@ ufw enable
 echo "\033[33mUFW firewall status\033[0m"
 ufw status
 
-# Against DDOS
 # add filters and edit nginx.conf
+# add limits.conf - which is included into nginx.conf http section
 echo "\033[33mSetting up nginx...\033[0m"
-cp config/nginx.conf /etc/nginx/nginx.conf
-chmod 644 /etc/nginx/nginx.conf
-cp config/nginx-conn-limit.conf /etc/fail2ban/filter.d/
-cp config/nginx-req-limit.conf /etc/fail2ban/filter.d/
-chmod 644 /etc/fail2ban/filter.d/nginx-conn-limit.conf
-chmod 644 /etc/fail2ban/filter.d/nginx-req-limit.conf
-echo "\033[33mReload nginx configuration\033[0m"
-service nginx restart
-
-# Fail2ban
+cp config/limits.conf /etc/nginx/conf.d/limits.conf
+chmod 644 /etc/nginx/conf.d/limits.conf
+# Fail2ban conf Against DDOS, ports scanning and protect sshd
 echo "\033[33mSetting up Fail2ban...\033[0m"
 cp config/jail.local /etc/fail2ban/
 chmod 644 /etc/fail2ban/jail.local
-echo "\033[33mReload Fail2ban configuration\033[0m"
-fail2ban-client reload
-echo "\033[33mFail2ban status\033[0m"
-service fail2ban status
-
-# for ports scan
-echo "\033[33mSetting up default/portsentry...\033[0m"
+# fail2ban with UFW
+cp config/ufw.conf /etc/fail2ban/action.d/ufw.conf
+chmod 644 /etc/fail2ban/action.d/ufw.conf
+# fail2ban filter
+cp config/ufwscanban.conf /etc/fail2ban/filter.d/ufwscanban.conf
+chmod 644 /etc/fail2ban/filter.d/ufwscanban.conf
+# Portsentry conf
+echo "\033[33mSetting up portsentry\033[0m"
 cp config/portsentry /etc/default/portsentry
 chmod 644 /etc/default/portsentry
-echo "\033[33mSetting up portsentry.conf\033[0m"
+cp config/portsentry.conf /etc/portsentry/portsentry.conf
 chmod 644 /etc/portsentry/portsentry.conf
-echo "\033[33mReload Portsentry configuration\033[0m"
-service portsentry restart
-echo "\033[Portsentry status\033[0m"
-service portsentry status
 
 # configure mail to root
 echo "\033[33mSetting up root emails\033[0m"
@@ -95,16 +97,27 @@ echo "0 0 * * * /etc/cron.d/scripts/survey.sh" >> mycron
 crontab mycron
 rm mycron
 
+echo "\033[33mReload nginx configuration\033[0m"
+service nginx restart
+echo "\033[33mReload Fail2ban configuration\033[0m"
+fail2ban-client reload
+echo "\033[33mFail2ban status\033[0m"
+service fail2ban status
+echo "\033[33mReload Portsentry configuration\033[0m"
+service portsentry restart
+echo "\033[Portsentry status\033[0m"
+service portsentry status
+
 echo "\033[33mAutomatic configuration done\033[0m"
 echo "\033[33m\n\nPlease complete the SSH configuration for the public key authentication by following those guidelines.\n\033[0m"
 echo "-- run the command without the $ --
 On host:
 $ ssh-keygen
-$ ssh-copy-id $PERSO@192.168.56.2
+$ ssh-copy-id $new_user@192.168.56.2
 or copy the key manually into ~/.ssh/Authorized_keys file
 [that hostname is corresponding to our setup]"
 ip a | grep enp0s8| grep inet
-echo "When it's done, to enable ssh public key login, edit /etc/ssh/sshd_config file
+echo "When it's done, to enable ssh public key username, edit /etc/ssh/sshd_config file
 $ sudo vim /etc/ssh/sshd_config
 uncomment and change those lines
   #PubkeyAuthentication yes
@@ -114,7 +127,7 @@ uncomment and change those lines
 then Restart service
 $ sudo service ssh restart
 You can now connect to remote-host via ssh by typing
-$ $PERSO@192.168.56.2 -p2223
+$ $new_user@192.168.56.2 -p2223
 "
 
 echo "\033[33mOn Then reboot, stop undesired services (you can see which one are running typing)
